9fe8bb06d28ebc22a5bb01b9fce11e937f7d8522
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-01-17 13:25:00 +0900

    forgot to commit files to call new mail account wizard with static config

---
+++ content/achook/triggerOverlay.js
@@ -0,0 +1,36 @@
+"use strict";
+
+window.addEventListener("load", function ACHook_triggerOverlay_init() {
+  window.removeEventListener("load", ACHook_triggerOverlay_init, false);
+
+  const { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
+
+  const { StaticConfig } = Cu.import("resource://achook-modules/StaticConfig.js", {});
+  const { Preferences } = Cu.import('resource://achook-modules/Preferences.js', {});
+
+  const Messages = {
+    _messages : new Preferences("extensions.achook."),
+    getLocalized: function (key, defaultValue) {
+      if (this._messages.has(key + ".override"))
+        key += ".override";
+      return this._messages.getLocalized(key, defaultValue);
+    }
+  };
+
+  var plainItem = document.getElementById("newMailAccountMenuItem");
+  var staticConfigItem = document.getElementById("newMailAccountMenuItem_achook_staticConfig");
+
+  if (StaticConfig.available && !plainItem.hidden) {
+    if (StaticConfig.always) {
+      staticConfigItem.setAttribute("hidden", true);
+    } else {
+      let label = Messages.getLocalized("newMailAccountMenuItem.label");
+      let accesskey = Messages.getLocalized("newMailAccountMenuItem.accesskey");
+      staticConfigItem.setAttribute("label", label.replace(/%domain%/gi, StaticConfig.domain));
+      staticConfigItem.setAttribute("accesskey", accesskey.replace(/%domain%/gi, StaticConfig.domain).charAt(0));
+      staticConfigItem.removeAttribute("hidden");
+    }
+  } else {
+    staticConfigItem.setAttribute("hidden", true);
+  }
+}, false);
