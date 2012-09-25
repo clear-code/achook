38c18d81b72547a4508ee07e23cadd7ec86dc70a
YUKI Hiroshi <shimoda@clear-code.com>
2012-09-25 18:43:07 +0900

    Remove obsolete pref

--- defaults/preferences/achook.js
+++ defaults/preferences/achook.js
@@ -1,7 +1,6 @@
 // pref("extensions.achook.staticConfig.domain", "example.com");
 // pref("extensions.achook.staticConfig.source", "chrome://achook/content/example.com.xml");
 // pref("extensions.achook.staticConfig.always", false);
-// pref("extensions.achook.staticConfig.required", false);
 // pref("extensions.achook.overwriteExistingAccount.incomingServer", true);
 // pref("extensions.achook.overwriteExistingAccount.outgoingServer", true);
 pref("extensions.achook.newMailAccountMenuItem.label", "chrome://achook/locale/achook.properties");
