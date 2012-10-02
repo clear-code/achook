ce39f7b5da99a58585ef05edfc3aba01018fdc42
YUKI Hiroshi <shimoda@clear-code.com>
2012-10-02 14:38:01 +0900

    Distribute codes under the MPL 2.0.

--- defaults/preferences/achook.js
+++ defaults/preferences/achook.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 // pref("extensions.achook.staticConfig.domain", "example.com");
 // pref("extensions.achook.staticConfig.source", "chrome://achook/content/example.com.xml");
 // pref("extensions.achook.disableGenericWizard", true);
--- modules/eval.js
+++ modules/eval.js
@@ -1,1 +1,5 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 try { __nc4_eval_result = eval(__nc4_eval_string); } catch (e) { __nc4_eval_error = e; }
