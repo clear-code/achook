a91150953338c917073a770d3e0cfd464d69c0ef
YUKI Hiroshi <shimoda@clear-code.com>
2012-11-27 14:33:55 +0900

    Add missing preference name

--- modules/PreferenceNames.js
+++ modules/PreferenceNames.js
@@ -7,6 +7,7 @@ let EXPORTED_SYMBOLS = ["PreferenceNames"];
 let PreferenceNames = {
   staticConfigDomain: "extensions.achook.staticConfig.domain",
   staticConfigSource: "extensions.achook.staticConfig.source",
+  disableGenericWizard: "extensions.achook.disableGenericWizard",
   disableNewEmailAccountCreation: "extensions.achook.disableNewEmailAccountCreation",
   overwriteExistingAccount_incomingServer: "extensions.achook.overwriteExistingAccount.incomingServer",
   overwriteExistingAccount_outgoingServer: "extensions.achook.overwriteExistingAccount.outgoingServer"
