a1555357889178acda9d70809fd3ed82335e1217
YUKI Hiroshi <shimoda@clear-code.com>
2013-11-19 12:15:42 +0900

    Add sample configuration for GMail

--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@ xpi: buildscript/makexpi.sh
 	cp buildscript/makexpi.sh ./
 	./makexpi.sh -n $(PACKAGE_NAME) -o
 	rm ./makexpi.sh
+	make -C gmail-config && cp gmail-config/gmail-config.xpi ./
 
 buildscript/makexpi.sh:
 	git submodule update --init
---
+++ gmail-config/defaults/preferences/gmail-config.js
@@ -0,0 +1,4 @@
+// configurations for achook
+pref("extensions.achook.staticConfig.domain", "gmail.com");
+pref("extensions.achook.staticConfig.source", "chrome://gmail-config/content/gmail.com.xml");
+pref("extensions.achook.staticConfig.separatedUsername", false);
