85c608e5e5aa050349c162a58534c23000c67b82
YUKI Hiroshi <shimoda@clear-code.com>
2014-08-13 12:37:39 +0900

    Add configuration example for POP3

---
+++ gmail-config-pop/defaults/preferences/gmail-config.js
@@ -0,0 +1,4 @@
+// configurations for achook
+pref("extensions.achook.staticConfig.gmail-pop.domain", "gmail.com");
+pref("extensions.achook.staticConfig.gmail-pop.source", "chrome://gmail-config-pop/content/gmail.com.xml");
+pref("extensions.achook.staticConfig.gmail-pop.separatedUsername", false);
